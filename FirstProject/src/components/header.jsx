4c9cba7cca2a798dd937c8ee9c91318674a226c4
Tahir-programmer <tsboy234@gmail.com>
2025-06-11 13:05:32 +0500

    Menu Modified

--- FirstProject/src/components/header.jsx
+++ FirstProject/src/components/header.jsx
@@ -12,7 +12,7 @@ function header() {
                 <li><a href="#contact" className="hover:underline">Contact</a></li>
                 <li><a href="#blog" className="hover:underline">Blog</a></li>
                 <li><a href="#projects" className="hover:underline">Help</a></li>
-                <li><a href="#contact-us">Contact Us</a></li>
+                <li><a href="#contact-us">CU</a></li>
             </ul>
         </nav>      
         </header>
